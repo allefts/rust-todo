2544376594871ad53449a7ed3b9d6ccfae028cb7
allefts <alleftsoares@gmail.com>
2023-07-21 14:06:53 -0500

    working? on todo

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
-use pancurses::{endwin, initscr, set_title, Input};
+use ncurses::*;
+
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
 
 fn main() {
     let window = initscr();
-    set_title("Rust Todo List");
-    window.refresh();
+
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+
+    addstr("Rust Todo List");
+    refresh();
 
     let mut quit = false;
+    let mut todos = vec!["Learn Rust", "Learn Nix", "Learn Vim Motions", "Buy a bread"];
+    let todo_curr: usize = 0;
 
     while !quit {
-        let user_input = window.getch();
-        match user_input {
-            Some(Input::Character(c)) => {
-                window.addch(c);
-            }
+    
+       for (idx, todo) in todos.iter().enumerate() {
+           let pair = {
+                if todo_curr == index {
+                    return HIGHLIGHT_PAIR
+                } else {
+                    return REGULAR_PAIR
+                }
+           }
+
+            mv(idx as i32, 0);
+            addstr(*todo);
+       }
+
+       refresh();
+
+       let key = getch();
+       match key as u8 as char {
+           'q' => quit = true,
             _ => {}
-        }
+       }
     }
 
     endwin();
